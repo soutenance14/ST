c0cfd74a2b69f47f9f66f21d113417f27e75462b
sout14 <soutenace14@gmail.com>
2021-08-23 20:34:54 +0200

    Update User Controller with forgottenPassword Management Fixes #4

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,9 +4,14 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\UserPasswordType;
+use App\Form\ForgottenPasswordType;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportException;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
@@ -15,15 +20,6 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
  */
 class UserController extends AbstractController
 {
-    // /**
-    //  * @Route("/", name="user_index", methods={"GET"})
-    //  */
-    // public function index(UserRepository $userRepository): Response
-    // {
-    //     return $this->render('user/index.html.twig', [
-    //         'users' => $userRepository->findAll(),
-    //     ]);
-    // }
 
     /**
      * @Route("/{id}", name="user_show", methods={"GET"})
@@ -36,7 +32,7 @@ class UserController extends AbstractController
     }
 
     /**
-     * @Route("/edit/password", name="user_edit", methods={"GET","POST"})
+     * @Route("/edit/password", name="user_edit_password", methods={"GET","POST"})
      */
     public function editPassword(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
     {
@@ -61,6 +57,51 @@ class UserController extends AbstractController
             'form' => $form,
         ]);
     }
+    
+    /**
+     * @Route("/forgotten/password", name="user_forgotten_password", methods={"GET","POST"})
+     */
+    public function forgottenPassword(Request $request, UserRepository $userRepository, MailerInterface $mailer): Response
+    {
+        $error_message ='';
+        $form = $this->createForm(ForgottenPasswordType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $email = $form->get('email')->getData();
+            $user = $userRepository->findOneByEmail($email);
+            if($user === null)
+            {
+                $error_message = 'Cet utilisateur n\'existe pas';
+                // $this->addFlash(
+                //     'danger',
+                //     "Cet utilisateur n\'existe pas"
+                // );
+            }
+            else
+            {
+                //generate and give a unique token to user
+                $user->setToken(md5(uniqid($user->getUsername(), true)));
+                $this->getDoctrine()->getManager()->flush();
+                try
+                {   
+                    $this->sendEmail($mailer, $user->getEmail(), $user->getToken());
+                    return $this->redirectToRoute('_profiler_home', [], Response::HTTP_SEE_OTHER);
+                }
+                catch(TransportException $e)
+                {
+                    $error_message = 'Impossible d\'envoyer le mail, veuillez réessayez ultérieurement.';
+                }
+            }
+        }
+
+        return $this->renderForm('user/forgotten_password.html.twig', [
+            // 'user' => $user,
+            'form' => $form,
+            'error_message' => $error_message,
+        ]);
+    }
 
     // /**
     //  * @Route("/delete", name="user_delete", methods={"POST"})
@@ -75,4 +116,16 @@ class UserController extends AbstractController
 
     //     return $this->redirectToRoute('user_index', [], Response::HTTP_SEE_OTHER);
     // }
+
+    private function sendEmail(MailerInterface $mailerInterface, $emailTo, $token)
+    {
+        $emailFrom = $this->getParameter('app.admin_email');
+        $link = '<a href="google.com"></a>';
+        $email=(new Email())
+        ->from($emailFrom)
+        ->to($emailTo)
+        ->subject('Link to reset password for Snow Tricks!')
+        ->text($link);
+        $mailerInterface->send($email);
+    }
 }
